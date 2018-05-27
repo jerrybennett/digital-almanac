c4dbf5e338050275e236e3f9d0be70e8311b5b4a
jerrybennett <jerrysbusiness@gmail.com>
2018-05-27 16:24:15 -0400

    add redux redux-thunk axios

--- src/index.js
+++ src/index.js
@@ -3,6 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware, compose } from "redux"
+import { Provider } from "react-redux"
+import thunk from "redux-thunk";
+import rootReducer from "./reducers/rootReducer"
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk),
+  // other store enhancers if any
+);
+const store = createStore(rootReducer, enhancer);
+
+console.log(store)
+console.log(store.getState())
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>, document.getElementById('root')
+);
 registerServiceWorker();
